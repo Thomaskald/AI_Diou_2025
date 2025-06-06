4b6c778b667c305f933c1056417ec1e291e342ce
thomas <thomaskald849@gmail.com>
2025-06-06 23:24:18 +0300

    started fit()

--- project2/linear_progression.py
+++ project2/linear_progression.py
@@ -1,3 +1,4 @@
+import numpy
 import numpy as np
 
 class LinearProgression:
@@ -7,3 +8,10 @@ class LinearProgression:
         self.b = None
         self.N = None
         self.p = None
+
+    def fit(self, X, y):
+        X = numpy.array(X)
+        y = numpy.array(y)
+
+        if len(X) != len(y):
+            raise ValueError("X and y must be compatible")
